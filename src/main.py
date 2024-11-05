308301a0b18a43a1b3c23519e3f56a8e64866335
Dylan <rattledew17@gmail.com>
2024-11-04 16:53:44 -0800

    added windows notification with timer

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
+from win10toast import ToastNotifier
+import time
 
+toast = ToastNotifier()
+
+def reminder() -> None:
+    print("Timer initiated.")
+    time.sleep(5) # every 30 minutes
+    print("Timer completed.")
+    
+    print("Message printed.")
+    toast.show_toast(
+        title="Hydrate!",
+        msg="Drink water, please.",
+        duration = 10,
+    )
+    print("Message deleted.")
 
 if __name__ == "__main__":
-    pass
+    reminder()
