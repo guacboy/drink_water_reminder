5c776743afc151ed62d42dc3565b354489ac0072
Dylan <rattledew17@gmail.com>
2024-11-20 11:15:45 -0800

    added tkinter set up

--- src/main.py
+++ src/main.py
@@ -1,20 +1,43 @@
 from win10toast import ToastNotifier
+from tkinter import *
 import time
 
 toast = ToastNotifier()
+root = Tk()
 
+# main window
+root.title("Drink Water Reminder")
+root.geometry("340x380")
+root.config(background="#454545")
+
+is_on = True
+
+# widget management
+class Util:
+    def create_button(window) -> Button:
+        return Button(window)
+
+# main app
+def main() -> None:
+    pass
+
+# reminder controls
 def reminder() -> None:
     print("Timer initiated.")
-    time.sleep(5) # every 30 minutes
+    time.sleep(1800) # delay before each notification
     print("Timer completed.")
     
     print("Message printed.")
     toast.show_toast(
         title="Hydrate!",
         msg="Drink water, please.",
-        duration = 10,
+        duration = 15,
     )
     print("Message deleted.")
+    
+    # calls itself if enabled
+    if is_on:
+        reminder()
 
 if __name__ == "__main__":
-    reminder()
+    root.mainloop()
